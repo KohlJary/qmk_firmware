21838470a5dfd2753acaf1963963398127a83e3b
Kohlbern Jary <kohlbern@gmail.com>
2022-09-14 17:04:09 -0400

    Tweaking L_1

--- keyboards/massdrop/alt/keymaps/KohlJary/keymap.c
+++ keyboards/massdrop/alt/keymaps/KohlJary/keymap.c
@@ -16,11 +16,11 @@ enum alt_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //Base layer
     [L_B] = LAYOUT_65_ansi_blocker(
-        G(KC_GRV),       KC_1,         KC_2,         KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,    KC_9,           KC_0,           KC_MINS,  KC_EQL, KC_BSPC, LT(L_S,KC_GRV),
-        GUI_T(KC_DEL),   KC_Q,         KC_W,         KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,           KC_P,           KC_LBRC, KC_RBRC, KC_BSLS, KC_VOLD,
-        CTL_T(KC_ESC),   KC_A,         KC_S,         KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,           TD(T_CN),        USR_QT,    ALT_T(KC_ENT), TD(T_HE),
-        KC_LSFT,         LT(L_1,KC_Z), LT(L_2,KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, LT(L_2,KC_DOT), LT(L_1,KC_SLSH),  RSFT_T(KC_TAB), KC_UP,   TD(T_UD),
-        LT(L_S,KC_BSPC), TD(T_CC),     C(KC_V),               KC_SPC,                                         DM_PLY1,        DM_PLY2, KC_LEFT, KC_DOWN, KC_RGHT
+        G(KC_GRV),       KC_1,         KC_2,         KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,    KC_9,           KC_0,            KC_MINS, KC_EQL,  KC_BSPC, LT(L_S,KC_GRV),
+        GUI_T(KC_DEL),   KC_Q,         KC_W,         KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,           KC_P,            KC_BSPC, KC_DEL,  KC_BSLS, KC_VOLD,
+        CTL_T(KC_ESC),   KC_A,         KC_S,         KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,           TD(T_CN),        USR_QT,    ALT_T(KC_ENT),  TD(T_HE),
+        KC_LSFT,         LT(L_1,KC_Z), LT(L_2,KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, LT(L_2,KC_DOT), LT(L_1,KC_SLSH), RSFT_T(KC_TAB),   KC_UP,   TD(T_UD),
+        LT(L_S,KC_BSPC), TD(T_CC),     C(KC_V),               KC_SPC,                                       DM_PLY1,         DM_PLY2, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     //Inverted number row layer
     [L_I] = LAYOUT_65_ansi_blocker(
@@ -40,11 +40,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     //Modifier layer 1 (macros, symbols, vim-key arrows, keyboard mouse commands)
     [L_1] = LAYOUT_65_ansi_blocker(
-        _______, _______, _______, _______,   _______,    _______,    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MUTE,
-        _______, S(KC_1), S(KC_2), S(KC_3),   S(KC_LBRC), S(KC_RBRC), _______, _______, LAMBDA,  AND_OR,  EQ_NEQ,  BRACES,  PARBRA,  TG(L_I),    KC_WH_U,
-        _______, S(KC_6), S(KC_5), S(KC_4),   S(KC_9),    S(KC_0),    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_MINS, KC_EQL,           G(KC_QUOT), KC_WH_D,
-        _______, S(KC_7), S(KC_8), S(KC_GRV), KC_LBRC,    KC_RBRC,    KC_BSPC, KC_DEL,  ELPS,    _______, KC_BSLS,          KC_BTN1, KC_MS_U,    KC_BTN2,
-        _______, _______, _______,                                    _______,                            DM_REC1, DM_REC2, KC_MS_L, KC_MS_D,    KC_MS_R
+        _______, _______, _______, _______,   _______,    _______,    _______,    _______,   _______, _______,    _______, _______, _______, _______,    KC_MUTE,
+        _______, S(KC_1), S(KC_2), S(KC_3),   S(KC_LBRC), S(KC_RBRC), S(KC_COMM), S(KC_DOT), LAMBDA,  AND_OR,     EQ_NEQ,  _______, _______, TG(L_I),    KC_WH_U,
+        _______, S(KC_6), S(KC_5), S(KC_4),   S(KC_9),    S(KC_0),    KC_LEFT,    KC_DOWN,   KC_UP,   KC_RGHT,    KC_MINS, KC_EQL,           G(KC_QUOT), KC_WH_D,
+        _______, S(KC_7), S(KC_8), S(KC_GRV), KC_LBRC,    KC_RBRC,    LAMBDA,     AND_OR,    EQ_NEQ,  S(KC_BSLS), KC_BSLS,          KC_BTN1, KC_MS_U,    KC_BTN2,
+        _______, _______, _______,                                    _______,                                    DM_REC1, DM_REC2, KC_MS_L, KC_MS_D,    KC_MS_R
     ),
     //Modifier layer 2 (F-keys, numbers, media controls)
     [L_2] = LAYOUT_65_ansi_blocker(
